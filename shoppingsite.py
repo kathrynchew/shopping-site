f24c634737a24dedb06493752f8931be68cd001b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-22 20:26:50 -0700

    Fixed add_to_cart route

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -98,7 +98,19 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+    if "cart" in session:
+        if melon_id in session["cart"]:
+            session["cart"][melon_id] += 1
+        else:
+            session["cart"][melon_id] = 1
+    else:
+        session["cart"] = {}
+        session["cart"][melon_id] = 1
+
+    print session["cart"]
+
+    flash("Your MELON IS IN THE CART! Success. :)")
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
